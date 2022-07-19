56605b0f82ea1032393e0d0a4a2a04753f964f3d
faizan alam <faizan.alam@sourcefuse.com>
2022-07-19 18:56:50 +0530

    Updated .env files

--- cdk/common/stacks/lambda.stack.ts
+++ cdk/common/stacks/lambda.stack.ts
@@ -13,6 +13,7 @@ export class LambdaStack extends TerraformStack {
       region: process.env.AWS_REGION,
       accessKey: process.env.AWS_ACCESS_KEY_ID,
       secretKey: process.env.AWS_SECRET_ACCESS_KEY,
+      profile: process.env.AWS_PROFILE,
       assumeRole: {
        roleArn: process.env.AWS_ROLE_ARN,
       }
