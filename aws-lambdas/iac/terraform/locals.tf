24446b7edd022e90ecd541187acac03e0f97644c
mayank0202 <mayank.sharma@sourcefuse.com>
2022-09-27 22:38:47 +0530

    made configurable sns kms

--- aws-lambdas/iac/terraform/locals.tf
+++ aws-lambdas/iac/terraform/locals.tf
@@ -1,4 +1,5 @@
 locals {
   sns_lambda_name = "sns-boilerplate-${random_id.this.hex}"
   sqs_lambda_name = "sqs-boilerplate-${random_id.this.hex}"
+  sns_kms_master_key_id = var.sns_kms_master_key_id == null ? "alias/aws/sns" : var.sns_kms_master_key_id
 }
