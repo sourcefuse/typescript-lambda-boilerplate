43e1eee9ad7092dec49a1b458a564fa5939cab25
shubham.sinha@sourcefuse.com <saurabh.kumar@verizonconnect.com>
2023-01-06 12:51:16 +0530

    resolve pr comments for terraform and provider versions

--- aws-lambdas/iac/terraform/lambda/main.tf
+++ aws-lambdas/iac/terraform/lambda/main.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = ">= 1.0.8"
+  required_version = "~> 1.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.20.1"
+      version = "~> 4.20"
     }
     archive = {
       source  = "hashicorp/archive"
-      version = "2.2.0"
+      version = "~> 2.2"
     }
   }
 }
